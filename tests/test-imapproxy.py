037865b75790bde7c70f6301a3c6baf6fee49afb
Shawn Yarbrough <shawn.yarbrough@gmail.com>
2012-10-15 07:17:06 -0500

    placeholder tests for imapproxy.py

---
+++ tests/test-imapproxy.py
@@ -0,0 +1,23 @@
+#|##############################################################################
+#|Copyright (c) 2012, The Green-Span Project. All rights reserved. This code is
+#|Open Source Free Software - redistribution and use in source and binary forms,
+#|with or without modification, are permitted under the Two Clause BSD License.
+#|##############################################################################
+#|File Created: 2012-10-14
+#|Author(s): Shawn Yarbrough,
+#|##############################################################################
+
+""" test suite for imapproxy.py """
+
+import unittest
+
+class TestImapProxy(unittest.testcase):
+
+    def setUp(self):
+        pass    # placeholder
+
+    def test_imap(self):
+        pass    # placeholder
+
+if __name__ == '__main__':
+    unittest.main()
